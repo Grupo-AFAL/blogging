9959fbac94c0ba344df23e41aa7963e39542f22f
Miguel Frias <migue_tj_frias@hotmail.com>
2022-05-10 09:52:04 -0700

    Configure generators

--- lib/blogging/engine.rb
+++ lib/blogging/engine.rb
@@ -1,5 +1,21 @@
 module Blogging
   class Engine < ::Rails::Engine
     isolate_namespace Blogging
+
+    config.generators do |g|
+      g.test_framework :rspec, fixture: true
+      g.view_specs      false
+      g.routing_specs   false
+      g.helper          false
+    end
+
+    initializer 'Blogging precompile hook' do |app|
+      if defined?(Sprockets)
+        app.config.assets.precompile += %w[
+          blogging/application.js
+          blogging/application.css
+        ]
+      end
+    end
   end
 end
