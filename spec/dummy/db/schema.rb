f0e08e2b8a3df2beed902bfdcf19b4cffbd36c65
Miguel Frias <migue_tj_frias@hotmail.com>
2022-05-11 10:47:44 -0700

    Use mobility and friendly id from host application

--- spec/dummy/db/schema.rb
+++ spec/dummy/db/schema.rb
@@ -10,44 +10,28 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_05_11_171324) do
+ActiveRecord::Schema[7.0].define(version: 2022_05_11_173452) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
-  create_table "friendly_id_slugs", force: :cascade do |t|
-    t.string "slug", null: false
-    t.integer "sluggable_id", null: false
-    t.string "sluggable_type", limit: 50
-    t.string "scope"
-    t.datetime "created_at"
-    t.index ["slug", "sluggable_type", "scope"], name: "index_friendly_id_slugs_on_slug_and_sluggable_type_and_scope", unique: true
-    t.index ["slug", "sluggable_type"], name: "index_friendly_id_slugs_on_slug_and_sluggable_type"
-    t.index ["sluggable_type", "sluggable_id"], name: "index_friendly_id_slugs_on_sluggable_type_and_sluggable_id"
-  end
-
-  create_table "mobility_string_translations", force: :cascade do |t|
-    t.string "locale", null: false
-    t.string "key", null: false
-    t.string "value"
-    t.string "translatable_type"
-    t.bigint "translatable_id"
+  create_table "blogging_posts", force: :cascade do |t|
+    t.integer "author_id", null: false
+    t.integer "status", default: 0
+    t.datetime "public_from"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.index ["translatable_id", "translatable_type", "key"], name: "index_mobility_string_translations_on_translatable_attribute"
-    t.index ["translatable_id", "translatable_type", "locale", "key"], name: "index_mobility_string_translations_on_keys", unique: true
-    t.index ["translatable_type", "key", "value", "locale"], name: "index_mobility_string_translations_on_query_keys"
   end
 
-  create_table "mobility_text_translations", force: :cascade do |t|
-    t.string "locale", null: false
-    t.string "key", null: false
-    t.text "value"
-    t.string "translatable_type"
-    t.bigint "translatable_id"
+  create_table "blogging_posts_tags", id: false, force: :cascade do |t|
+    t.bigint "blogging_post_id", null: false
+    t.bigint "blogging_tag_id", null: false
+    t.index ["blogging_post_id", "blogging_tag_id"], name: "index_blogging_posts_tags_on_post_id_and_tag_id"
+    t.index ["blogging_tag_id", "blogging_post_id"], name: "index_blogging_posts_tags_on_tag_id_and_post_id"
+  end
+
+  create_table "blogging_tags", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.index ["translatable_id", "translatable_type", "key"], name: "index_mobility_text_translations_on_translatable_attribute"
-    t.index ["translatable_id", "translatable_type", "locale", "key"], name: "index_mobility_text_translations_on_keys", unique: true
   end
 
 end
