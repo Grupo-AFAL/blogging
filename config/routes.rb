4810044352012cd80bc7affc5a44d01449a1999b
Miguel Frias <migue_tj_frias@hotmail.com>
2022-05-12 10:33:11 -0700

    Add routes for admin posts and tags

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,9 @@ Blogging::Engine.routes.draw do
 
   resources :posts, path: '/', only: %i[index show]
   resources :tags, only: %i[show]
+
+  namespace :admin do
+    resources :posts
+    resources :tags
+  end
 end
