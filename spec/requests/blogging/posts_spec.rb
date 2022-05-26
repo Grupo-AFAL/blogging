e46bf6886e1603dbfa84f2d8d2ae2b4588500edf
Sergio Lopez <sergio@softnya.com>
2022-05-26 10:45:12 -0500

    Test de posts

--- spec/requests/blogging/posts_spec.rb
+++ spec/requests/blogging/posts_spec.rb
@@ -2,7 +2,20 @@
 
 require 'rails_helper'
 
-RSpec.describe 'Posts', type: :request do
+RSpec.describe 'Published Posts', type: :request do
+  fixtures 'blogging/posts', 'blogging/tags', 'action_text/rich_texts', :users,
+           :mobility_string_translations, :mobility_text_translations
+
+  let(:blog_post) { blogging_posts(:published) }
+  let(:image) { fixture_file_upload('waldo.jpg', 'image/jpeg') }
+
   describe '#index' do
+    it { expect(get(posts_path)).to render_template(:index) }
+  end
+
+  describe '#show' do
+    before { attach_image(blog_post, :cover_image, 'waldo.jpg') }
+
+    it { expect(get(post_path(blog_post))).to render_template(:show) }
   end
 end
