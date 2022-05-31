2e8a09900eee894826b49c22d7662720b88c6aa8
Miguel Frias <migue_tj_frias@hotmail.com>
2022-05-31 11:18:07 -0700

    Fix tests

--- spec/requests/blogging/posts_spec.rb
+++ spec/requests/blogging/posts_spec.rb
@@ -9,13 +9,13 @@ RSpec.describe 'Published Posts', type: :request do
   let(:blog_post) { blogging_posts(:published) }
   let(:image) { fixture_file_upload('waldo.jpg', 'image/jpeg') }
 
+  before { attach_image(blog_post, :cover_image, 'waldo.jpg') }
+
   describe '#index' do
     it { expect(get(posts_path)).to render_template(:index) }
   end
 
   describe '#show' do
-    before { attach_image(blog_post, :cover_image, 'waldo.jpg') }
-
     it { expect(get(post_path(blog_post))).to render_template(:show) }
   end
 end
