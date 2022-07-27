0fdb921fb7cc873487b27e9138c6cb7a855eb294
Miguel Frias <31300198+MiguelFrias97@users.noreply.github.com>
2022-07-27 12:48:48 -0700

    Add Tag to post status

--- app/helpers/blogging/application_helper.rb
+++ app/helpers/blogging/application_helper.rb
@@ -7,5 +7,11 @@ module Blogging
     def icon_tag(icon, **options)
       render Bali::Icon::Component.new(icon, **options)
     end
+
+    def status_color(status)
+      return :info if status.to_sym == :published
+
+      :light
+    end
   end
 end
